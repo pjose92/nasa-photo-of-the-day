6e2b978b8329b31db26588d9f94efe213b2c865c
Jose <pjose14.jp38@yahoo.com>
2020-02-07 17:48:34 -0600

    added style to the header. added NASA logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React from "react";
 import TheSpaceInfo from './components/TheSpaceInfo';
 import "./App.css";
+import styled from "styled-components";
 
-function App() {
+const AppWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  max-width: 900px;
+  margin: 0 auto;
+  box-shadow: #aaa 0 10px 60px;
+  background: black;
+  padding: 2px;
+  color: white;
+`;
+
+function App  () {
   return (
-    <div className="App">
-      <h1> NASA Photo of The Day</h1>
+    <header>
+      <AppWrapper>
+      <h1>NASA Photo of The Day</h1>
+        <img
+          src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg"
+          alt="Nasa Logo"
+        />
+      </AppWrapper>
       <TheSpaceInfo />
-    </div>
+    </header>
+
   );
-}
+};
 
 export default App;
+
+// function App() {
+//   return (
+    
+//     <div className="App">
+//       <h1> NASA Photo of The Day</h1>
+      // <TheSpaceInfo />
+//     </div>
+//   );
+// }
+
+// export default App;
